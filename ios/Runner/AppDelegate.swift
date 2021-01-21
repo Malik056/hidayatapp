46e908c38fb6284d69fb28df466deab9be096554
Muhammad Taha <tahamahmoodmalik@gmail.com>
2021-01-21 13:35:34 +0500

    Updating

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,17 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    }
+    private func registerPlugins(registry: FlutterPluginRegistry) {
+        if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+           FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+        }
+    }
 }
